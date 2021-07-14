577e7b8e0f77ef36e122a504d32c5e4134408f5a
AntiAlex <saxara_a@mail.ru>
2021-07-15 01:54:43 +0300

    Added black detection

--- asix.cpp
+++ asix.cpp
@@ -146,6 +146,44 @@ void drawCubeWireframe(
     cv::line(image, imagePoints[6], imagePoints[7], cv::Scalar(255, 0, 0), 3);
 }
 
+void detectBlackRectangles(Mat& image, std::vector<std::vector<cv::Point>>& stickers) {
+    cv::Mat image_hsv;
+    std::vector< std::vector<cv::Point> > contours;
+    cv::cvtColor(image, image_hsv, cv::COLOR_BGR2HSV ); 
+    cv::Mat tmp_img(image.size(), CV_8U);
+
+    cv::inRange(image_hsv, cv::Scalar(0, 0, 0, 0), cv::Scalar(180, 255, 30, 0), tmp_img);
+
+    cv::dilate(tmp_img,tmp_img,cv::Mat(), cv::Point(-1,-1), 3);
+    cv::erode(tmp_img,tmp_img,cv::Mat(), cv::Point(-1,-1), 1);
+    cv::findContours(tmp_img, stickers, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
+    for (uint i = 0; i<contours.size(); i++) {
+        cv::Mat sticker;
+        cv::Rect rect=cv::boundingRect(contours[i]);
+        image(rect).copyTo(sticker);
+        cv::rectangle(image, rect, cv::Scalar(0, 250, 0), 2);
+        stickers.push_back(sticker); 
+    }
+}
+
+bool cmpPointX(const cv::Point &a, const cv::Point &b) {
+    return a.x < b.x;
+}
+
+bool cmpPointY(const cv::Point &a, const cv::Point &b) {
+    return a.y < b.y;
+}
+
+static bool stickers_comp(std::vector<cv::Point>& st1, std::vector<cv::Point>& st2) {
+    int first_x = abs((*min_element(st1.begin(), st1.end(), cmpPointX)).x - (*max_element(st1.begin(), st1.end(), cmpPointX)).x);
+    int first_y = abs((*min_element(st1.begin(), st1.end(), cmpPointY)).y - (*max_element(st1.begin(), st1.end(), cmpPointY)).y);
+
+    int sec_x = abs((*min_element(st2.begin(), st2.end(), cmpPointX)).x - (*max_element(st2.begin(), st2.end(), cmpPointX)).x);
+    int sec_y = abs((*min_element(st2.begin(), st2.end(), cmpPointY)).y - (*max_element(st2.begin(), st2.end(), cmpPointY)).y);
+
+    return (first_x * first_y < sec_x * sec_y);
+}
+
 
 int main()
 {
@@ -158,6 +196,7 @@ int main()
     }
 
     namedWindow("Detection", cv::WindowFlags::WINDOW_AUTOSIZE);
+    std::vector<std::vector<cv::Point>> stickers;
 
     while (1)
     {
@@ -176,8 +215,7 @@ int main()
         qrDecoder.detect(frame, bbox);
         std::string data = qrDecoder.detectAndDecode(frame, bbox, rectifiedImage);
 
-         if (data.length() > 0)
-        {
+        if (data.length() > 0) {
             cout << "Decoded Data : " << data << endl;
         //     display(frame, bbox);
         //     rectifiedImage.convertTo(rectifiedImage, CV_8UC3);
@@ -189,6 +227,18 @@ int main()
         //     cout << "QR Code not detected" << endl;
         //     imshow("Detection", frame);
         // }
+    
+        detectBlackRectangles(frame, stickers);
+        auto st = max_element(stickers.begin(), stickers.end(), stickers_comp);
+
+        cv::Point sticker1; 
+        sticker1.x = (*min_element((*st).begin(), (*st).end(), cmpPointX)).x;
+        sticker1.y = (*min_element((*st).begin(), (*st).end(), cmpPointY)).y;
+        cv::Point sticker2; 
+        sticker2.x = (*max_element((*st).begin(), (*st).end(), cmpPointX)).x;
+        sticker2.y = (*max_element((*st).begin(), (*st).end(), cmpPointY)).y;
+
+        cv::rectangle(frame, Rect(sticker1, sticker2),cv::Scalar(0,250,0), 2);
 
         vector<Point3f> obj_coords;
         obj_coords.push_back(Point3f(0,0,0));
