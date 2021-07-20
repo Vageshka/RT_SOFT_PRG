a9317eb4414ee82f0d24958e8c43a1a8ac373ef7
AntiAlex <saxara_a@mail.ru>
2021-07-20 18:48:28 +0300

    Display as rect + finding contours ver of homography update

--- main.cpp
+++ main.cpp
@@ -14,54 +14,33 @@ using namespace cv;
 using namespace std;
 using namespace cv::xfeatures2d;
 
-const int MAX_FEATURES = 4000;
-const float GOOD_MATCH_PERCENT = 0.15f;
 const static double QR_CODE_WIDTH = 0.06;
 const static double MAP_WIDTH = 0.21;
 const static double MAP_HEIGHT = 0.3;
 const static double QR_CENTER_X = 0.115;
 const static double QR_CENTER_Y = 0.051;
+const static std::string path = "./images_User_Facing/";
 
-void alignImages(Mat& im1, Mat& im2, Mat& im1Reg, Mat& h) {
-  // Convert images to grayscale
-  Mat im1Gray, im2Gray;
-  cvtColor(im1, im1Gray, COLOR_BGR2GRAY);
-  cvtColor(im2, im2Gray, COLOR_BGR2GRAY);
 
-  // Variables to store keypoints and descriptors
-  vector<KeyPoint> keypoints1, keypoints2;
-  Mat descriptors1, descriptors2;
+void findCorners(const vector<Point>& contours, const Point& points[4]) {
+    Point &x_max=points[2], &x_min=points[0], &y_max=points[3], &y_min=points[1];
+    x_max = x_min = y_max = y_min = contours[0];
 
-  // Detect ORB features and compute descriptors.
-  Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
-  orb->detectAndCompute(im1Gray, Mat(), keypoints1, descriptors1);
-  orb->detectAndCompute(im2Gray, Mat(), keypoints2, descriptors2);
+    for (auto contour : contours){
+        if (x_max.x < cur.x) x_max = contour;
+        if (x_min.x > cur.x) x_min = contour;
+        if (y_max.y < cur.y) y_max = contour;
+        if (y_min.y > cur.y) y_min = contour;
+    }
+}
 
-  // Match features.
-  vector<DMatch> matches;
-  Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-  matcher->match(descriptors1, descriptors2, matches, Mat());
+void display(Mat &im, Mat &bbox) {
+    for (int i = 0; i < bbox.rows; i++) {
+        line(im, Point2i(bbox.at<float>(i, 0), bbox.at<float>(i, 1)), 
+        Point2i(bbox.at<float>((i + 1) % n, 0), bbox.at<float>((i + 1) % n, 1)), Scalar(255, 0, 0), 3);
+    }
 
-  // Sort matches by score
-  sort(matches.begin(), matches.end());
-
-  // Remove not so good matches
-  const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
-  matches.erase(matches.begin()+numGoodMatches, matches.end());
-
-  // Extract location of good matches
-  vector<Point2f> points1, points2;
-
-  for (auto match : matches) {
-    points1.push_back( keypoints1[match.queryIdx].pt);
-    points2.push_back( keypoints2[match.trainIdx].pt);
-  }
-
-  // Find homography
-  h = findHomography( points1, points2, RANSAC);
-
-  // Use homography to warp image
-  warpPerspective(im1, im1Reg, h, im2.size());
+    imshow("Detection", im);
 }
 
 void getCameraParams(Mat& cameraMatrix, Mat& distCoeffs) {
@@ -81,17 +60,13 @@ void getCameraParams(Mat& cameraMatrix, Mat& distCoeffs) {
         objp.push_back(Point3f(j, i, 0));
     }
 
-
     // Extracting path of individual image stored in a given directory
     vector<cv::String> images;
-    // Path of the folder containing checkerboard images
-    std::string path = "./images_User_Facing/";
-
     glob(path, images);
 
     Mat frame, gray;
     // vector to store the pixel coordinates of detected checker board corners 
-    vector<Point2f> corner_pts;
+    vector<Point2f> cornerPts;
     bool success;
 
     // Looping over all the images in the directory
@@ -101,7 +76,7 @@ void getCameraParams(Mat& cameraMatrix, Mat& distCoeffs) {
 
         // Finding checker board corners
         // If desired number of corners are found in the image then success = true  
-        success = findChessboardCorners(gray, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), corner_pts, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
+        success = findChessboardCorners(gray, cv::Size(CHECKERBOARD[0], CHECKERBOARD[1]), cornerPts, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
         
         /* 
         * If desired number of corner are detected,
@@ -112,10 +87,10 @@ void getCameraParams(Mat& cameraMatrix, Mat& distCoeffs) {
             TermCriteria criteria(TermCriteria::EPS | TermCriteria::COUNT, 30, 0.001);
             
             // refining pixel coordinates for given 2d points.
-            cornerSubPix(gray, corner_pts, cv::Size(11, 11), cv::Size(-1, -1), criteria);
+            cornerSubPix(gray, cornerPts, cv::Size(11, 11), cv::Size(-1, -1), criteria);
             
             objpoints.push_back(objp);
-            imgpoints.push_back(corner_pts);
+            imgpoints.push_back(cornerPts);
         }
     }
 
@@ -127,47 +102,7 @@ void getCameraParams(Mat& cameraMatrix, Mat& distCoeffs) {
     * and corresponding pixel coordinates of the 
     * detected corners (imgpoints)
     */
-    calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows,gray.cols), cameraMatrix, distCoeffs, R, T);
-}
-
-void drawCubeWireframe(InputOutputArray& image, const InputArray& cameraMatrix, const InputArray& distCoeffs, InputArray& rvec, InputArray& tvec, float l) {
-
-    CV_Assert(
-        image.getMat().total() != 0 &&
-        (image.getMat().channels() == 1 || image.getMat().channels() == 3)
-    );
-    CV_Assert(l > 0);
-    float half_l = l / 2.0;
-
-    // project cube points
-    vector<Point3f> axisPoints;
-    axisPoints.push_back(Point3f(l, l, half_l));
-    axisPoints.push_back(Point3f(0, l, half_l));
-    axisPoints.push_back(Point3f(0, 0, half_l));
-    axisPoints.push_back(Point3f(l, 0, half_l));
-    axisPoints.push_back(Point3f(l, l, 0));
-    axisPoints.push_back(Point3f(0, l, 0));
-    axisPoints.push_back(Point3f(0, 0, 0));
-    axisPoints.push_back(Point3f(l, 0, 0));
-
-    vector<Point2f> imagePoints;
-    projectPoints(
-        axisPoints, rvec, tvec, cameraMatrix, distCoeffs, imagePoints
-    );
-
-    // draw cube edges lines
-    line(image, imagePoints[0], imagePoints[1], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[0], imagePoints[3], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[0], imagePoints[4], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[1], imagePoints[2], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[1], imagePoints[5], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[2], imagePoints[3], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[2], imagePoints[6], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[3], imagePoints[7], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[4], imagePoints[5], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[4], imagePoints[7], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[5], imagePoints[6], Scalar(255, 0, 0), 3);
-    line(image, imagePoints[6], imagePoints[7], Scalar(255, 0, 0), 3);
+    calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows, gray.cols), cameraMatrix, distCoeffs, R, T);
 }
 
 void drawTrajectory(const vector<Point2f>& trajectory, Mat& image) {
@@ -182,7 +117,6 @@ int main() {
 
     vector<Point2f> trajectory;
     Mat cameraMatrix, distCoeffs;
-    Mat prevGoodFrame = trjImage;
 
     getCameraParams(cameraMatrix, distCoeffs);
     VideoCapture cap(1);
@@ -203,36 +137,59 @@ int main() {
             break;
         }
 
+        Mat gray, lap, blured;
+
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        Laplacian(gray, lap, CV_32F, 3);
+        convertScaleAbs(lap, gray);
+        GaussianBlur(gray, blured, Size(5,5), 0);
+        threshold(blured, blured, 0, 255, THRESH_BINARY | THRESH_OTSU);
+        morphologyEx(blured, blured, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3,3)), Point(-1,-1), 2);
+        erode(blured, blured, Mat(), Point(-1,-1), 3);
+        dilate(blured, blured, Mat(), Point(-1,-1), 2);
+        morphologyEx(blured, blured, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3,3)), Point(-1,-1), 2);
+        
+        vector<vector<Point>> contours;
+        findContours(blured, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        if (contours.size()){
+            Rect max_br = boundingRect(contours[0]);
+            uint num = 0;
+            for (uint i = 0; i < contours.size(); ++i){
+                Rect br = boundingRect(contours[i]);
+                if (br.area() > max_br.area()){
+                    max_br = br;
+                    num = i;
+                }
+            }
+
+            Point corners[4];
+            find_corners(contours[num], corners);
+            vector<Point> p, x;
+            for(int i = 0; i < 4; i++)
+                x.push_back(corners[i]);
+            p.push_back(Point(50,250)); p.push_back(Point(50, 50)); 
+            p.push_back(Point(250,50)); p.push_back(Point(250, 250)); 
+            Mat h = findHomography( x, p, RANSAC), im; 
+            warpPerspective(frame, im, h, im.size());
+            imshow("Homography", im);
+        }
+
         QRCodeDetector qrDecoder = QRCodeDetector();
         
         Mat bbox, rectifiedImage;
         qrDecoder.detect(frame, bbox);
         // std::string data = qrDecoder.detectAndDecode(frame, bbox, rectifiedImage);
 
-        if (bbox.empty()) {
-            // Registered image will be resotred in imgAliged. 
-            // The estimated homography will be stored in h. 
-            Mat imgAligned, h;
-            alignImages(frame, prevGoodFrame, imgAligned, h);
-
-            qrDecoder.detect(imgAligned, bbox);
-            std::string data = qrDecoder.detectAndDecode(imgAligned, bbox, rectifiedImage);
-            imshow("Homography", imgAligned);
-        } else {
-            prevGoodFrame = frame;
-        }
-
         if (!bbox.empty()) {
-            vector<Point3f> obj_coords;
-            obj_coords.push_back(Point3f(0, 0, 0));
-            obj_coords.push_back(Point3f(0, QR_CODE_WIDTH, 0));
-            obj_coords.push_back(Point3f(QR_CODE_WIDTH, QR_CODE_WIDTH, 0));
-            obj_coords.push_back(Point3f(QR_CODE_WIDTH, 0, 0));
+            vector<Point3f> objCoords;
+            objCoords.push_back(Point3f(0, 0, 0));
+            objCoords.push_back(Point3f(0, QR_CODE_WIDTH, 0));
+            objCoords.push_back(Point3f(QR_CODE_WIDTH, QR_CODE_WIDTH, 0));
+            objCoords.push_back(Point3f(QR_CODE_WIDTH, 0, 0));
             Mat R,T;
 
-            solvePnP(obj_coords, bbox, cameraMatrix, distCoeffs, R, T);
-            // aruco::drawAxis(frame, cameraMatrix, distCoeffs, R, T, 0.07);
-            drawCubeWireframe(frame, cameraMatrix, distCoeffs, R, T, QR_CODE_WIDTH);
+            solvePnP(objCoords, bbox, cameraMatrix, distCoeffs, R, T);
+            display(im, bbox);
             Mat Rt, Rtt;
             Rodrigues(R, Rt);
             transpose(Rt, Rtt);
